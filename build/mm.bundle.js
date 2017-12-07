8ec1abcc2fdbc6cb285e12668806041c30fceb13
shockw0w <33080031+shockw0w@users.noreply.github.com>
2017-12-07 10:49:18 +0600

    Add files via upload

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -71096,7 +71096,7 @@ angular.module('mm.core')
     "appname": "Miras Astana app",
     "desktopappname": "Miras Astana app",
     "versioncode" : "2021",
-    "versionname" : "0.0.2",
+    "versionname" : "0.0.3",
     "cache_expiration_time" : 300000,
     "default_lang" : "en",
     "languages": {"en": "English","fr" : "Français", "ru": "Русский"},
